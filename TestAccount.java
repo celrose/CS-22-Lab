93229a9de3b13b2e7a44fedcd47e1b22a288a1f5
Rocelle <annecadavos@yahoo.com>
2016-09-20 18:34:42 +0800

    Update TestAccount.java

--- TestAccount.java
+++ TestAccount.java
@@ -1,19 +1,26 @@
 public class TestAccount{
 	public static void main(String[] args) {
-		Account account = new Account(123, 100.00);
-		System.out.println(account);
 
-		account.credit(12.0);
-		System.out.println(account.balance);
-		System.out.println(account);
+		Account account1 = new Account();
+		Account account2 = new Account(51523);
+		Account account3 = new Account(142131, 100.00);
 
-		account.debit(13.11);
-		System.out.println(account.balance);
-		System.out.println(account);
+		System.out.println(account1);
+		System.out.println(account2);
+		System.out.println(account3);
 
-		Account account2 = new Account(2113, 50.00);
-		account2.debit(51.00);
-		System.out.println(account2);		
+		//account1.accountNumber = 8782;
+		account1.credit(12.0);
+		account1.debit(10.0);
 
+		account2.credit(100.0);
+		account2.debit(50.0);
+
+		account3.credit(1.0);
+		account3.debit(25.13);
+
+		System.out.println(account1);
+		System.out.println(account2);
+		System.out.println(account3);
 	}
 }
